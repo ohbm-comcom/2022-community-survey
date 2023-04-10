0d60d5d6dea92a4b2d6e5fef8616eab875f827d2
Elizabeth DuPre <emd222@cornell.edu>
2023-04-10 16:30:22 -0700

    Move model analysis to MyST MD

---
+++ model_outputs.md
@@ -0,0 +1,208 @@
+---
+jupytext:
+  formats: ipynb,md:myst
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.14.5
+kernelspec:
+  display_name: Python 3 (ipykernel)
+  language: python
+  name: python3
+---
+
+```{code-cell} ipython3
+import json
+
+from survey_analysis import format_data, fit_model
+```
+
+```{code-cell} ipython3
+responses = format_data('public_survey_data.csv')
+```
+
+```{code-cell} ipython3
+# now let's run some basic analyses to check against SurveyMonkey outputs
+responses['is_member'].value_counts(normalize=True)
+```
+
+```{code-cell} ipython3
+responses['geographic_region'].value_counts(normalize=True)
+```
+
+```{code-cell} ipython3
+responses['career_stage'].value_counts(normalize=True)
+```
+
+```{code-cell} ipython3
+# for the other questions, we can load in our sidecar JSON
+# with question-level metadata
+with open('levels.json') as f:
+    queries = json.load(f)
+```
+
+```{code-cell} ipython3
+print(f'There are {len(queries.keys())} questions to consider.') 
+print(queries.keys())
+```
+
+```{code-cell} ipython3
+question = 'job_board_access'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'job_board_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'email_access'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'email_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'blog_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'blog_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'twitter_access'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'twitter_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'podcast_access'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'podcast_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'facebook_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'youtube_access'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'youtube_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'linkedin_access'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'linkedin_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'ondemand_access'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'ondemand_content'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'content_platform'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'sig_platform'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
+
+```{code-cell} ipython3
+question = 'content_importance'
+scale_values = queries[question]
+
+res = fit_model(responses, question, scale_values)
+print(res.summary())
+```
--- survey_analysis.py
+++ survey_analysis.py
@@ -49,83 +49,94 @@ def fit_model(responses, query, levels):
     return res
 
 
-demographics = {
-    'Are you a member of OHBM?': 'is_member',
-    'What geographic region are you currently located in?': 'geographic_region',
-    'What is your current career status?': 'career_stage'
-}
-rename_questions = {
-    'How would you describe your access to the OHBM job board?': 'job_board_access',
-    'How would you describe the content on the OHBM job board?': 'job_board_content',
-    'How would you describe your access to emails from OHBM?': 'email_access',
-    'How would you describe the content in OHBM emails?': 'email_content',
-    'How would you describe your access to the OHBM blog?': 'blog_access',
-    'How would you describe the content in the OHBM blog?': 'blog_content',
-    'How would you describe your access to OHBM Twitter?': 'twitter_access',
-    'How would you describe the content on OHBM Twitter?': 'twitter_content',
-    'How would you describe your access to the NeuroSalience podcast?': 'podcast_access',
-    'How would you describe the content in the NeuroSalience podcast?': 'podcast_content',
-    'How would you describe your access to OHBM Facebook?': 'facebook_access',
-    'How would you describe the content in OHBM Facebook?': 'facebook_content',
-    'How would you describe your access to OHBM YouTube?': 'youtube_access',
-    'How would you describe the content in OHBM YouTube?': 'youtube_content',
-    'How would you describe your access to OHBM LinkedIn?': 'linkedin_access',
-    'How would you describe the content in OHBM LinkedIn?': 'linkedin_content',
-    'How would you describe your access to the OHBM OnDemand?': 'ondemand_access',
-    'How would you describe the content in OHBM OnDemand?': 'ondemand_content',
-    'Which of the following platforms do you use to access OHBM content? When applicable, a direct link to the platform is provided next to each option. Please check all options that apply.': 'content_platform',
-    'Do you currently follow any of the following OHBM Special Interest Groups (SIG) platforms? When applicable, a direct link to the platform is provided next to each option. Please check all options that apply.': 'sig_platform',
-    'How important is each of these types of content to you?': 'content_importance'
-}
+def format_data(survey_data_path):
+    """
+    """
+    # Load in the data. This spreadsheet is (almost) direct from SurveyMonkey,
+    # though IP addresses, access dates, and free-text responses were scrubbed
+    # to anonymize respondents.
 
-# Load in the data. This spreadsheet is (almost) direct from SurveyMonkey,
-# though IP addresses, access dates, and free-text responses were scrubbed
-# to anonymize respondents.
+    df = pd.read_csv(survey_data_path, sep=',', index_col='Unnamed: 0')
 
-df = pd.read_csv('public_survey_data.csv', sep=',', index_col='Unnamed: 0')
+    # SurveyMonkey provides an odd nesting of responses when exporting results.
+    # We'd like to convert this structure to a pandas MultiIndex data frame.
+    # First, let's find question indices -- adapted from https://stackoverflow.com/a/49584888
 
-# SurveyMonkey provides an odd nesting of responses when exporting results.
-# We'd like to convert this structure to a pandas MultiIndex data frame.
-# First, let's find question indices -- adapted from https://stackoverflow.com/a/49584888
+    indices = [i for i, c in enumerate(df.columns) if not c.startswith('Unnamed')]
+    repeats = [len(df.columns[i:j]) for i, j in zip_longest(indices, indices[1:])]
 
-indices = [i for i, c in enumerate(df.columns) if not c.startswith('Unnamed')]
-repeats = [len(df.columns[i:j]) for i, j in zip_longest(indices, indices[1:])]
+    # We can pair each possible response with its associated question...
+    matched_questions = []
+    for question, n_rep in zip(df.columns[indices], repeats):
+        matched_questions += [question] * n_rep
 
-# We can pair each possible response with its associated question...
-matched_questions = []
-for question, n_rep in zip(df.columns[indices], repeats):
-    matched_questions += [question] * n_rep
+    df.columns = matched_questions
+    df.index.name = 'participant_id'
 
-df.columns = matched_questions
-df.index.name = 'participant_id'
+    # Now we'll rename our columns to ease analysis--
+    demographics = {
+        'Are you a member of OHBM?': 'is_member',
+        'What geographic region are you currently located in?': 'geographic_region',
+        'What is your current career status?': 'career_stage'
+    }
+    rename_questions = {
+        'How would you describe your access to the OHBM job board?': 'job_board_access',
+        'How would you describe the content on the OHBM job board?': 'job_board_content',
+        'How would you describe your access to emails from OHBM?': 'email_access',
+        'How would you describe the content in OHBM emails?': 'email_content',
+        'How would you describe your access to the OHBM blog?': 'blog_access',
+        'How would you describe the content in the OHBM blog?': 'blog_content',
+        'How would you describe your access to OHBM Twitter?': 'twitter_access',
+        'How would you describe the content on OHBM Twitter?': 'twitter_content',
+        'How would you describe your access to the NeuroSalience podcast?': 'podcast_access',
+        'How would you describe the content in the NeuroSalience podcast?': 'podcast_content',
+        'How would you describe your access to OHBM Facebook?': 'facebook_access',
+        'How would you describe the content in OHBM Facebook?': 'facebook_content',
+        'How would you describe your access to OHBM YouTube?': 'youtube_access',
+        'How would you describe the content in OHBM YouTube?': 'youtube_content',
+        'How would you describe your access to OHBM LinkedIn?': 'linkedin_access',
+        'How would you describe the content in OHBM LinkedIn?': 'linkedin_content',
+        'How would you describe your access to the OHBM OnDemand?': 'ondemand_access',
+        'How would you describe the content in OHBM OnDemand?': 'ondemand_content',
+        'Which of the following platforms do you use to access OHBM content? When applicable, a direct link to the platform is provided next to each option. Please check all options that apply.': 'content_platform',
+        'Do you currently follow any of the following OHBM Special Interest Groups (SIG) platforms? When applicable, a direct link to the platform is provided next to each option. Please check all options that apply.': 'sig_platform',
+        'How important is each of these types of content to you?': 'content_importance'
+    }
 
-responses = pd.melt(
-    df.iloc[1:].reset_index().rename({**demographics, **rename_questions}, axis=1), 
-    id_vars=['participant_id'] + list(demographics.values()), 
-    value_name='response', 
-    var_name='question'
-)
+    responses = pd.melt(
+        df.iloc[1:].reset_index().rename({**demographics, **rename_questions}, axis=1), 
+        id_vars=['participant_id'] + list(demographics.values()), 
+        value_name='response', 
+        var_name='question'
+    )
 
-# We'll need to set up a coding scheme for our demographic data
-responses['is_member'] = responses.is_member.astype(bool)
-responses['geographic_region'] = responses.geographic_region.astype('category')
-responses['career_stage'] = responses.career_stage.astype('category')
+    # We'll need to set up a coding scheme for our demographic data
+    responses['is_member'] = responses.is_member.astype(bool)
+    responses['geographic_region'] = responses.geographic_region.astype('category')
+    responses['career_stage'] = responses.career_stage.astype('category')
 
-# now let's run some basic analyses to check against SurveyMonkey outputs
-responses['is_member'].value_counts(normalize=True)
-responses['geographic_region'].value_counts(normalize=True)
-responses['career_stage'].value_counts(normalize=True)
+    return responses
 
-with open('./levels.json') as f:
-    queries = json.load(f)
 
-for q in queries:
-    levels = queries[q]
-    print(f'Now fitting a model for {q}....')
-    res = fit_model(responses, q, levels)
-    print(res.summary())
-    print(np.exp(res.params))  # odds ratios
-    print()
+if __name__ == "__main__":
+    responses = format_data('public_survey_data.csv')
+
+    # now let's run some basic analyses to check against SurveyMonkey outputs
+    responses['is_member'].value_counts(normalize=True)
+    responses['geographic_region'].value_counts(normalize=True)
+    responses['career_stage'].value_counts(normalize=True)
+
+    # load in our JSON with question-level metadata for available responses
+    with open('levels.json') as f:
+        queries = json.load(f)
+
+    for q in queries:
+        levels = queries[q]
+        print(f'Now fitting a model for {q}....')
+        res = fit_model(responses, q, levels)
+        print(res.summary())
+        print(np.exp(res.params))  # odds ratios
+        print()
 
 
 # FIXME : blog_access
